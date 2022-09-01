34f0781f9f9b80b27c9e7482e1a205e7dc7d28f8
Wonka-source <dan.eff117@gmail.com>
2022-09-01 21:38:19 +0000

    add play game function, welcome message and player name prompt

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+# class Board:
+#     pass
+
+
+def play_game():
+    print("-" * 35) 
+    print("Welcome to a Battleships Game")
+    print("Board size: 5. Number of ships: 4")
+    print("Top left corner is row: 0, col: 0")
+    print("-" * 35) 
+
+    player_name = input("Please enter your name: ")
+    print(player_name)
+
+    
+play_game()
