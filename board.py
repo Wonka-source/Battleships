8de87349e5ce256cd5d26963e2fbadb7bcad0a40
Wonka-source <dan.eff117@gmail.com>
2022-09-07 15:52:42 +0000

    move functions into board class, put board class in its own module

---
+++ board.py
@@ -0,0 +1,164 @@
+from random import randint
+
+
+class Board:
+    """
+    Main board class. Sets board size, the number of ships,
+    the name and board type (computer or player).
+    """
+
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+        # generates a list of all the coords of the player board
+        # this is used for computers remaining turns/guesses
+        if type == "player":
+            self.remaining_player_board = [
+                (x, y) for x in range(size) for y in range(size)
+                ]
+
+    def print(self):
+        """
+        Function for printing the board
+        """
+        for row in self.board:
+            print(" ".join(row))
+
+    def guess(self, a_valid_guess):
+        """
+        Function for adding guesses to the board
+        """
+        x = a_valid_guess[0]
+        y = a_valid_guess[1]
+
+        self.guesses.append((x, y))
+
+        if (x, y) in self.ships:
+            self.board[x][y] = "*"
+            return "Hit"
+        else:
+            self.board[x][y] = "X"
+            return "Miss"
+
+    def populate_board(self):
+        """
+        populates ships to the computer's and the player's
+        board
+        """
+        num_of_ships_placed = 0
+
+        while self.num_ships > num_of_ships_placed:
+
+            battleship_row = random_point(self.size)
+            battleship_col = random_point(self.size)
+            if (battleship_row, battleship_col) not in self.ships:
+                self.ships.append((battleship_row, battleship_col))
+                num_of_ships_placed += 1
+                if self.type == "player":
+                    self.board[battleship_row][battleship_col] = "@"
+
+    def check_for_win(self):
+        """
+        Checks for a winner by checking if all the coordinates of self.ship
+        list are in self.guesses list.
+        https://thispointer.com/python-check-if-a-list-contains-all-the-elements-of-another-list/
+        """
+        return all(elm in self.guesses for elm in self.ships)
+
+    def invalid_guess(self, x, y):
+        """
+        Takes an x and a y (coord) and checks if it has already been
+        guessed (if it is already present in the board.guesses class
+        attribute of the given board (argument)).
+        """
+        if (x, y) in self.guesses:
+            return True
+        return False
+
+    def take_guess(self, player_score):
+        """
+        When called prompts the player to input their guess which is stored
+        as an x and a y variable, respectively. Then uses the invalid_guess()
+        to check if the guess is invalid (prompts the player "already guessed
+        if it is invalid) and re-askes the player to input until it is
+        valid. Then Prints the valid guess to the board.
+        """
+        x = take_coord("row", self.size)
+        y = take_coord("column", self.size)
+        while self.invalid_guess(x, y):
+            print("ALREADY GUESSED!")
+            x = take_coord("row", self.size)
+            y = take_coord("column", self.size)
+        print("#" * 35)
+        hit_miss = self.guess((x, y))
+        print(f"\nPLAYER: {x, y} {hit_miss}!")
+        if hit_miss == "Hit":
+            player_score += 1
+        print("score:", player_score)
+        return player_score
+
+    def random_computer_guess(self, computer_score):
+        """
+        Used to generate a random guess for the computer. Takes "player board"
+        and uses "remaining_player_board" class attribute (witch is a list of
+        tuples [all possible coordinates]) and picks a random point along its
+        length. Then uses the pop() to pull it out (so it is then no longer
+        remaining within in the the list of choices) and stores the val in a
+        "xy" var (the computers guess). Then prints the guess to the players
+        board.
+        """
+        # https://www.w3schools.com/Python/python_lists_remove.asp
+        x_y = self.remaining_player_board.pop(
+            random_point(len(self.remaining_player_board))
+            )
+        hit_miss = self.guess(x_y)
+        print(f"\nCOMPUTER: {x_y[0], x_y[1]} {hit_miss}!")
+        if hit_miss == "Hit":
+            computer_score = computer_score + 1
+        print("score:", computer_score)
+        input("\nPress enter to continue:\n")
+        print("-" * 35)
+        return computer_score
+
+
+def valid_coord(coord, size):
+    """
+    Takes a string (coord) and checks if it is a number and within range
+    of the board size. Returns False if it is valid and True if it is
+    invalid.
+    """
+    return (coord > -1 and coord < size)
+
+
+def take_coord(row_column, size):
+    """
+    Takes the x or the y coord from the player. Then uses the invalid_coord()
+    to check if it is a valid coordinates (ie a num and within the range of the
+    board size). If it is not valid the player will be prompt to input again.
+    Returns the coord as an int.
+
+    """
+    while True:
+        try:
+            coord = int(input(f"Guess a {row_column}:\n").strip())
+            if not valid_coord(coord, size):
+                raise ValueError(f"Values must be between 0 and {size - 1}")
+
+        except ValueError:
+            print(f"Values must be between 0 and {size - 1}")
+            continue
+
+        return coord
+
+
+def random_point(size):
+    """
+    Helper function to return a random integer between 0 and size
+    """
+    return randint(0, size - 1)
--- run.py
+++ run.py
@@ -1,82 +1,4 @@
-from random import randint
-
-
-class Board:
-    """
-    Main board class. Sets board size, the number of ships,
-    the name and board type (computer or player).
-    """
-
-    def __init__(self, size, num_ships, name, type):
-        self.size = size
-        self.board = [["." for x in range(size)] for y in range(size)]
-        self.num_ships = num_ships
-        self.name = name
-        self.type = type
-        self.guesses = []
-        self.ships = []
-
-        # generates a list of all the coords of the player board
-        # this is used for computers remaining turns/guesses
-        if type == "player":
-            self.remaining_player_board = [
-                (x, y) for x in range(size) for y in range(size)
-                ]
-
-    def print(self):
-        """
-        Function for printing the board
-        """
-        for row in self.board:
-            print(" ".join(row))
-
-    def guess(self, a_valid_guess):
-        """
-        Function for adding guesses to the board
-        """
-        x = a_valid_guess[0]
-        y = a_valid_guess[1]
-
-        self.guesses.append((x, y))
-
-        if (x, y) in self.ships:
-            self.board[x][y] = "*"
-            return "Hit"
-        else:
-            self.board[x][y] = "X"
-            return "Miss"
-
-    def populate_board(self):
-        """
-        populates ships to the computer's and the player's
-        board
-        """
-        num_of_ships_placed = 0
-
-        while self.num_ships > num_of_ships_placed:
-
-            battleship_row = random_point(self.size)
-            battleship_col = random_point(self.size)
-            if (battleship_row, battleship_col) not in self.ships:
-                self.ships.append((battleship_row, battleship_col))
-                num_of_ships_placed += 1
-                if self.type == "player":
-                    self.board[battleship_row][battleship_col] = "@"
-
-    def check_for_win(self):
-        """
-        Checks for a winner by checking if all the coordinates of self.ship
-        list are in self.guesses list.
-        https://thispointer.com/python-check-if-a-list-contains-all-the-elements-of-another-list/
-        """
-        return all(elm in self.guesses for elm in self.ships)
-
-
-def random_point(size):
-    """
-    Helper function to return a random integer between 0 and size
-    """
-    return randint(0, size - 1)
+from board import Board
 
 
 def valid_board_size(size):
@@ -137,99 +59,10 @@ def take_ships(size):
                 raise ValueError(
                     f"Number must be between {min_ships} and {max_ships}."
                     )
-
         except ValueError:
             continue
 
         return ships
-
-
-def valid_coord(coord, size):
-    """
-    Takes a string (coord) and checks if it is a number and within range
-    of the board size. Returns False if it is valid and True if it is
-    invalid.
-    """
-    return (coord > -1 and coord < size)
-
-
-def take_coord(row_column, size):
-    """
-    Takes the x or the y coord from the player. Then uses the invalid_coord()
-    to check if it is a valid coordinates (ie a num and within the range of the
-    board size). If it is not valid the player will be prompt to input again.
-    Returns the coord as an int.
-
-    """
-    while True:
-        try:
-            coord = int(input(f"Guess a {row_column}:\n").strip())
-            if not valid_coord(coord, size):
-                raise ValueError(f"Values must be between 0 and {size - 1}")
-
-        except ValueError:
-            print(f"Values must be between 0 and {size - 1}")
-            continue
-
-        return coord
-
-
-def invalid_guess(x, y, board):
-    """
-    Takes an x and a y (coord) and checks if it has already been
-    guessed (if it is already present in the board.guesses class
-    attribute of the given board (argument)).
-    """
-    if (x, y) in board.guesses:
-        return True
-    return False
-
-
-def take_guess(board, player_score):
-    """
-    When called prompts the player to input their guess which is stored
-    as an x and a y variable, respectively. Then uses the invalid_guess() to
-    check if the guess is invalid (prompts the player "already guessed
-    if it is invalid) and re-askes the player to input until it is
-    valid. Then Prints the valid guess to the board.
-    """
-    x = take_coord("row", board.size)
-    y = take_coord("column", board.size)
-    while invalid_guess(int(x), int(y), board):
-        print("ALREADY GUESSED!")
-        x = take_coord("row", board.size)
-        y = take_coord("column", board.size)
-    print("#" * 35)
-    hit_miss = board.guess((x, y))
-    print(f"\nPLAYER: {x, y} {hit_miss}!")
-    if hit_miss == "Hit":
-        player_score += 1
-    print("score:", player_score)
-    return player_score
-
-
-def random_computer_guess(board, computer_score):
-    """
-    Used to generate a random guess for the computer. Takes "player board"
-    and uses "remaining_player_board" class attribute (witch is a list of
-    tuples [all possible coordinates]) and picks a random point along its
-    length. Then uses the pop() to pull it out (so it is then no longer
-    remaining within in the the list of choices) and stores the val in a
-    "xy" var (the computers guess). Then prints the guess to the players
-    board.
-    """
-    # https://www.w3schools.com/Python/python_lists_remove.asp
-    x_y = board.remaining_player_board.pop(
-        random_point(len(board.remaining_player_board))
-        )
-    hit_miss = board.guess(x_y)
-    print(f"\nCOMPUTER: {x_y[0], x_y[1]} {hit_miss}!")
-    if hit_miss == "Hit":
-        computer_score += 1
-    print("score:", computer_score)
-    input("\nPress enter to continue:\n")
-    print("-" * 35)
-    return computer_score
 
 
 def play_game(computer_board, player_board, player_score, computer_score):
@@ -245,8 +78,8 @@ def play_game(computer_board, player_board, player_score, computer_score):
     player_board.print()
     print("\nComputer's Board:")
     computer_board.print()
-    player_score = take_guess(computer_board, player_score)
-    computer_score = random_computer_guess(player_board, computer_score)
+    player_score = computer_board.take_guess(player_score)
+    computer_score = player_board.random_computer_guess(computer_score)
     if (player_board.check_for_win()):
         print("\n\nCOMPUTER WINS\n\n")
         new_game()
